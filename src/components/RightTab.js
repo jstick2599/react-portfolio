f3a31726ba41ae8556ec4a82b070eaf5de43ae71
jstick2599 <58639465+jstick2599@users.noreply.github.com>
2024-09-24 08:50:07 -0700

    import images for RightTab.js

--- src/components/RightTab.js
+++ src/components/RightTab.js
@@ -1,5 +1,10 @@
 import React, {useState} from 'react';
-import './RightTab.css';
+import github from '../imageLinks/github-logo.png';
+import linkedin from "../imageLinks/linkedin-logo.png";
+import moon from "../imageLinks/moon-logo.png";
+import sun from "../imageLinks/sun-logo.png";
+
+
 
 
 function RightTab({ theme, setTheme }){
@@ -25,24 +30,43 @@ function RightTab({ theme, setTheme }){
     
 
 
+function RightTab({ theme, setTheme }) {
+  const [isHover, setIsHover] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHover(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHover(false);
+  };
+
+  const handleMoonClick = () => {
+    setTheme(false);
+  };
+
+  const handleSunClick = () => {
+    setTheme(true);
+  };
+
   return (
     <div className="rightTabDiv">
-      <a href="https://github.com/jstick2599">
-        <img src="/imageLinks/github-logo.png" alt="github logo" className='github'/>
+      <a href="https://github.com/jstick2599" className='github'>
+        <img src={github} alt="github logo" className='github' />
       </a>
-      <a href="https://www.linkedin.com/in/joshua-stickles-980b60195/">
-        <img src="../../public/imageLinks/linkedin-logo.png" alt="linkedin logo" className='linkedin'/>
+      <a href="https://www.linkedin.com/in/joshua-stickles-980b60195/" className='linkedin'>
+        <img src={linkedin} alt="linkedin logo" className='linkedin' />
       </a>
       <div className='themeLogo'>
-      {theme ?<img src="../../public/imageLinks/moon-logo.png" alt="moon logo" className='moon' onClick={handleMoonClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}/>
-      :<img src="../../public/imageLinks/sun-logo.png" alt="sun logo" className='sun' onClick={handleSunClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}/>}
-      {isHover && <div className="hover">
-        <h4 className='popup'>Toggle Theme</h4>
-      </div> }
+        {theme ? (
+          <img src={moon} alt="moon logo" className='moon' onClick={handleMoonClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} />
+        ) : (
+          <img src={sun} alt="sun logo" className='sun' onClick={handleSunClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} />
+        )}
+        {isHover && <h4 className='popup'>Toggle Theme</h4>}
       </div>
     </div>
-      
   );
-}
+}}
 
 export default RightTab;
