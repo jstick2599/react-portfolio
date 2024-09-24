5455e7a6f1f625bcd0db5ac7bee299d4951cbf3a
jstick2599 <58639465+jstick2599@users.noreply.github.com>
2024-09-24 08:34:04 -0700

    test commit for Home.js

--- src/sections/home/Home.js
+++ src/sections/home/Home.js
@@ -40,6 +40,7 @@ const Home = forwardRef(({props, theme}, ref) => {
       <div className='color' style={colorStyle}>
         <img align="left" src={selfie}  alt="My Portrait" className='selfie'/>
       </div>
+      <h1>Delete soon</h1>
     </div>
   );
 
