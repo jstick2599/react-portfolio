9ba04523b50691290c2bccb3ee13f921d2063640
Joshua <jstick2599@gmail.com>
2026-01-20 14:59:53 -0700

    added versefacts.com to projects and removed education project

--- src/sections/projects/Projects.js
+++ src/sections/projects/Projects.js
@@ -1,5 +1,5 @@
 import React, {forwardRef} from 'react';
-import education from "../../imageLinks/education-project.png";
+import verseFacts from "../../imageLinks/versefactsImg.png";
 import encapsulate from "../../imageLinks/encapsulate.png";
 import keysight from "../../imageLinks/keysight-dashboard.png";
 import './Projects.css';
@@ -14,15 +14,15 @@ const Projects  = forwardRef(({props, theme}, ref) => {
         <h1 className="projectsTitle">Projects</h1>
         <div className='projectsContent'>
           <div className='projectsImage'>
-            <h1 className='educationTitle'>Education Project</h1>
-            <img src={education} alt="Education Project" className='educationProject'/>
+            <h1 className='verseFactsTitle'>Verse Facts</h1>
+            <img src={verseFacts} alt="Verse Facts Project" className='verseFactsProject'/>
             <h1 className='encapsulateTitle'>Encapsulate</h1>
             <img src={encapsulate} alt="Encapsulate Project" className='encapsulateProject'/>
             <h1 className='keysightTitle'>Keysight</h1>
             <img src={keysight} alt="Keysight Project" className='capstoneProject'/>
           </div>
           <div className='projectsDescription'>
-            <h2 className='educationDescription'>Education-themed full-stack web application built during UC Merced's HackMerced 2022 hackathon. It was built using ReactJS and Spring Boot</h2>
+            <h2 className='verseFactsDescription'>It is a business that I started that gives Bible verse facts generator with articles. It gives the users facts about how the bible is proven true by science/logic. It was built using ReactJS.</h2>
             <h2 className='encapsulateDescription'>Full Stack social media application. It was built using ReactJS and SQLite</h2>
             <h2 className='keysightDescription'>Capstone project made for our clients, Jira. It is a full Stack data extraction and visualization web application. It was built using ReactJS and Spring Boot</h2>
           </div>
